bcb3ab73c55f8ca7b5348a85079d4d05f88dcb09
Arnd R. Strube <a.strube@wind-soft.de>
2015-02-24 14:49:22 +0100

    RunAllTests2.cpp: Use of real code, namespace, fix bug

--- RunAllTests2.cpp
+++ RunAllTests2.cpp
@@ -2,22 +2,25 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockSupport.h"
 
-extern "C" {
-    
 /// C production code /////////////////////////////////////
-long Used_add(long a, long b);
-long Used_subtract(long a, long b);
-
-long User_calculate(long a, long b) {
-    long temp1 = Used_add(a, a);
-    long temp2 = Used_subtract(b, a);
-    return Used_subtract(temp1, temp2);
+namespace C {
+    long Used_add(long a, long b) { return a + b; }
+    long Used_subtract(long a, long b) { return a - b; }
 }
 
 /// Dummies ///////////////////////////////////////////////
 struct Used {
     virtual long add(long, long) { return 0l; }
     virtual long subtract(long, long) { return 0l; }
+};
+
+struct Used_Real : public Used {
+    virtual long add(long a, long b) override {
+        return C::Used_add(a, b);
+    }
+    virtual long subtract(long a, long b) override {
+        return C::Used_subtract(a, b);
+    }
 };
 
 struct Used_Mock : public Used {
@@ -27,7 +30,7 @@ struct Used_Mock : public Used {
              .withParameter("b", b);
         return mock().returnValue().getLongIntValue();
     }
-    virtual long Used_subtract_Mock(long a, long b) {
+    virtual long subtract(long a, long b) override {
         mock().actualCall("Used_subtract")
              .withParameter("a", a)
              .withParameter("b", b);
@@ -49,19 +52,42 @@ struct Used_Stub : public Used {
     }
 };
 
-static Used used;
+static Used_Real used_real;
+static Used used_dummy;
+static Used_Stub used_stub;
+static Used_Mock used_mock;
+static Used *used = &used_dummy;
 
 /// Fake proxies called by production code ///////////////
-long Used_add(long a, long b) {
-    return used.add(a, b);
-}
-long Used_subtract(long a, long b) {
-    return used.subtract(a, b);
+extern "C" long Used_add(long a, long b) {
+    return used->add(a, b);
 }
 
-} // extern "C"
+extern "C" long Used_subtract(long a, long b) {
+    return used->subtract(a, b);
+}
+
+/// C production code /////////////////////////////////////
+
+extern "C"  long User_calculate(long a, long b) {
+        long temp1 = Used_add(a, a);
+        long temp2 = Used_subtract(b, a);
+        return Used_subtract(temp1, temp2);
+}
 
 /// Tests /////////////////////////////////////////////////
+TEST_GROUP(Used_withReal){
+    void setup() override {
+        UT_PTR_SET(used, &used_real);
+    }
+}; 
+TEST(Used_withReal, add) {
+    LONGS_EQUAL(5678, Used_add(5555, 123));
+}
+TEST(Used_withReal, subtract) {
+    LONGS_EQUAL(1, Used_subtract(-34567, -34568));
+}
+
 TEST_GROUP(Used_withDummy) {}; 
 TEST(Used_withDummy, add) {
     LONGS_EQUAL(0, Used_add(5555, 123));
@@ -72,8 +98,7 @@ TEST(Used_withDummy, subtract) {
 
 TEST_GROUP(User_withUsedStubCode) {
     void setup() override {
-//        UT_PTR_SET(sUsed_Fakes.add, Used_add_Stub);
-//        UT_PTR_SET(sUsed_Fakes.subtract, Used_subtract_Stub);
+        UT_PTR_SET(used, &used_stub);
     }
 };
 TEST(User_withUsedStubCode, calculate) {
@@ -82,11 +107,11 @@ TEST(User_withUsedStubCode, calculate) {
 
 TEST_GROUP(User_withUsedMockCode) {
     void setup() override {
-//        UT_PTR_SET(sUsed_Fakes.add, Used_add_Mock);
- //       UT_PTR_SET(sUsed_Fakes.subtract, Used_subtract_Mock);
+        UT_PTR_SET(used, &used_mock);
     }
     void teardown() {
         mock().clear();
+        mock().checkExpectations();
     }
 };
 TEST(User_withUsedMockCode, calculate) {
@@ -94,28 +119,6 @@ TEST(User_withUsedMockCode, calculate) {
           .withParameter("a", 57)
           .withParameter("b", 57)
           .andReturnValue(114);
-    mock().expectOneCall("Used_subtract")
-          .withParameter("a", 312)
-          .withParameter("b", 57)
-          .andReturnValue(255);
-    mock().expectOneCall("Used_subtract")
-          .withParameter("a", 114)
-          .withParameter("b", 255)
-          .andReturnValue(-141);
-    LONGS_EQUAL(-141, User_calculate(57, 312));
-    mock().checkExpectations();
-}
-
-TEST_GROUP(User_withUsedMixed) {
-    void setup() override {
-//        UT_PTR_SET(sUsed_Fakes.add, Used_add_Stub);
-//        UT_PTR_SET(sUsed_Fakes.subtract, Used_subtract_Mock);
-    }
-    void teardown() override {
-        mock().clear();
-    }
-};
-TEST(User_withUsedMixed, calculate) {
     mock().expectOneCall("Used_subtract")
           .withParameter("a", 312)
           .withParameter("b", 57)
